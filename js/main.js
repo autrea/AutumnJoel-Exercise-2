fab8e2185b07b2c9ed9b1e1d4bf3389b7e1ac8e7
Autumn Read <autumn.read@uleth.ca>
2019-02-24 19:37:20 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var panel_0 = document.getElementById("panel_0");
+
+var panel_left = document.getElementById("panel_left");
+var panel_left__text = document.getElementById("panel_left__text");
+var panel_left__img = document.getElementById("panel_left__img");
+
+var panel_right = document.getElementById("panel_right");
+var panel_right__text = document.getElementById("panel_right__text");
+var panel_right__img = document.getElementById("panel_right__img");
+
+var panel_right__chosen = false;
